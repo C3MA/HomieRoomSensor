2059403f38c8b50e6eb7c2fe8a394b88732c842b
Ollo <Ollo@host>
2025-05-11 21:15:16 +0200

    Enhanced simulation with dynamic values

--- host/simulateMPPT75-10.py
+++ host/simulateMPPT75-10.py
@@ -24,6 +24,9 @@ with serial.Serial(serialDevice, 19200, timeout=1)  as ser:
 
     # some dummy values
     battery_volt=12000
+    battery_current = 1
+    panel_volt = 13000
+    panel_power = 2
 
     # Main Loop
     while (True):
@@ -37,11 +40,16 @@ with serial.Serial(serialDevice, 19200, timeout=1)  as ser:
         if (updateSerial):
             lastUpdate=int(time.time())
             # Send the status string
-            outStr="SER#\tHQ2202K3VD9\nV\t{0:d}\nChecksum	f\r\n".format(battery_volt)
+            outStr="SER#\tHQ2202K3VD9\nV\t{0:d}\nI\t{1:d}\nVPV\t{2:d}\nPPV\t{3:d}\nChecksum	f\r\n".format(battery_volt, battery_current, panel_volt, panel_power)
+            # Other values
+            # CS\t{4:d}\nMPPT\t{5:d}\nERR\t{6:d}\nLOAD\t{7}\nIL\t{8:d}\nH19\t{9:d}\nH20\t{10:d}\nH21\t{11:d}\nH22\t{12:d}\nH23\t{13:d}\nHSDS\t{14:d}\n
+
             # write a string
             ser.write(outStr.encode(encoding='UTF-8'))
             print(colored(outStr.rstrip(), "white"))
             time.sleep(0.2)
+            panel_volt = panel_volt + 1
+            panel_power = panel_power + 1
     
     ser.close()             # close port
 
