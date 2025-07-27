897fc7881e72155d206e529fd47a8cc177e1e3c8
Ollo <Ollo@host>
2025-07-27 13:20:48 +0200

    Display current flashed version in Firmware name

--- include/HomieSettings.h
+++ include/HomieSettings.h
@@ -12,9 +12,20 @@
 #ifndef HOMIE_SETTINGS
 #define HOMIE_SETTINGS
 
-#define HOMIE_FIRMWARE_NAME     "RoomSensor"
-#define HOMIE_FIRMWARE_VERSION  "2.4.5"
+#if BME680
+#define FIRMWARE_FEATURE1   "_Bme680"
+#else
+#define FIRMWARE_FEATURE1   ""
+#endif
 
+#if VICTRON
+#define FIRMWARE_FEATURE2   "_Victron"
+#else
+#define FIRMWARE_FEATURE2   ""
+#endif
+
+#define HOMIE_FIRMWARE_NAME     "RoomSensor" FIRMWARE_FEATURE1 FIRMWARE_FEATURE2
+#define HOMIE_FIRMWARE_VERSION  "2.4.5"
 
 #define SERIAL_BAUDRATE 19200
 
